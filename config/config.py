35cf25a5832b68e8f9e014adf94efbfd23e42284
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-06-22 02:56:20 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -5,7 +5,7 @@ OWNER_ID = int(os.getenv("OWNER_ID", "1784606556"))
 
 USERNAME_BOT = os.getenv(
     "USERNAME_BOT",
-    "smallubot",
+    "riaa_userbot",
 )
 
 LOGS_DATABASE_UBOT = int(os.getenv("LOGS_DATABASE_UBOT", "-1001805411267"))
@@ -26,34 +26,34 @@ ADMINS = [int(x) for x in (os.environ.get("ADMINS", "966484443 1784606556").spli
 
 MONGO_BOT = os.getenv(
     "MONGO_BOT",
-    "mongodb+srv://smalbot:smalbot@cluster0.gee3fd7.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://riabot:riabot@cluster0.cxeyxiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 )
 
 MONGO_UBOT = os.getenv(
     "MONGO_UBOT",
-    "mongodb+srv://ubot:ubot@cluster0.fueonvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "",
 )
 
 MONGO_CLIENT = os.getenv(
     "MONGO_CLIENT",
-    "mongodb+srv://client:client@cluster0.8rgnrxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "",
 )
 
 
 # MONGODB EXPIRED
 MONGO_EXPIRED = os.getenv(
     "MONGO_EXPIRED",
-    "mongodb+srv://expired:expired@cluster0.rnwj7ho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "",
 )
 
 # MONGODB REVOKE
 MONGO_UTILS = os.getenv(
     "MONGO_UTILS",
-    "mongodb+srv://tobrut:tobrut@cluster0.wswopzc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "m",
 )
 
 # MONGODB TOPCMD
 MONGO_TOPCMD = os.getenv(
     "MONGO_TOPCMD",
-    "mongodb+srv://tobrut:tobrut@cluster0.sagdcy8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://riatopcmd:riatopcmd@cluster0.zzikqb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 )
